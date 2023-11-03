3207ca50605290a3b26309653daeb898859cb049
bagiourangjawa <144506198+bagiourangjawa@users.noreply.github.com>
2023-11-03 11:20:05 +0700

    Update HomeView.vue
    update visi misi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,20 +22,42 @@ import Sidebar from "../components/Sidebar.vue";
         <li>
           1. Meningkatkan Profesionalisme Pelayanan Publik;
           <ul>
-            <li>Meningkatkan kualitas pelayanan masyarakat dan penyelenggaraan pemerintahan desa; </li>
+            <li>a. Meningkatkan kualitas pelayanan masyarakat dan penyelenggaraan pemerintahan desa; </li>
+            <li>b. Meningkatkan Profesionalitas dan kinerja apartur desa demi tercapainya perencanaan pembangunan desa; </li>
+            <li>c. Meningkatkan dan mengembangkan partisipasi masyarakat dalam pembangunan desa. </li>
             </ul>
         </li>
         <li>
           2. Meningkatkan Kesehatan masyarakat melalui Program Pemerintah;
+          <ul>
+            <li>a. Mendorong masyarakat untuk sadar kesehatan dengan memfasilitasi jaminan kesehatan masyarakat melalui program pemerintah; </li>
+            <li>b. Meningkatkan sarana dan prasarana kesehatan di desa melaui pemberdayaan Posyandu dan pembangunan pos kesehatan di desa.</li>
+            </ul>
         </li>
         <li>
           3. Mewujudkan sarana dan prasarana infrastruktur yang nyaman;
+          <ul>
+            <li>a. Meningkatkan akses infrastruktur jalan desa, jalan lingkungan, jalan pertanian dan akses jalan wisata; </li>
+            <li>b. Meningkatkan akses infrastruktur jalan desa, jalan lingkungan, jalan pertanian dan akses jalan wisata; </li>
+            <li>c. Memberikan tempat tinggal yang layak bagi masyarakat melalui program pemerintah berupa Rehabalitasi rumah tidak layak huni; </li>
+            <li>d. Meningkatkan pembangunan sarana dan prasarana olahraga. </li>
+            </ul>
         </li>
         <li>
           4. Mewujudkan kesadaran masyarakat terhadap pentingnya pendidikan;
+         <ul>
+            <li>a. Mempermudah akses masyarakat untuk memperoleh pendidikan; </li>
+            <li>b. Mendorong masyarakat untuk peran serta mendukung pemerintah dengan wajib belajar 12 Tahun;</li>
+            <li>c. Mendorong pembangunan sarana dan prasarana pendidikan baik melalui Dinas terkait maupun APBdesa. </li>
+            </ul>
         </li>
         <li>
           5.Mewujudkan masyarakat yang agamis dan mandiri;
+        <ul>
+            <li>a. Meningkatkan peran serta masyarakat dalam kegiatan keagamaan; </li>
+            <li>b. Mendukung kegiatan keagamaan di lingkungan masyarakat melalui bantuan keuangan desa dan kegiatan bulanan keagamaan dimasing-masing wilayah;</li>
+            <li>c. Mendorong kegiatan untuk mengembangkan potensi desa baik melalui pemberdayaan masyarakat yang meliputi kelembagaan desa, bumdes, serta peran serta masyarakat sebagai upaya untuk menghasilkan Pendapatan Asli Desa. </li>
+            </ul>
         </li>
         <li>
         </li>
